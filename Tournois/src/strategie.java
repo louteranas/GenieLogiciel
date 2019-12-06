02d6570296c4297554c58fd985dbd20ee4614996
louteranas <louteranas@gmail.com>
2019-12-06 16:12:53 +0100

    got it

--- Tournois/src/strategie.java
+++ Tournois/src/strategie.java
@@ -1,4 +1,23 @@
 
 public class strategie {
+	private String nom;
+	
+	public strategie() {
+		this.setNom("coucou");
+	}
+	
+	public String getAction(String pastAction) {
+		if(pastAction != "coucou") {
+			return "coucou";
+		}
+		return pastAction;
+	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
